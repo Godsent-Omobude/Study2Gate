673b0a47f910339c194e5d8737788a476d44ed80
Godsent-Omobude <godsentomobude@gmail.com>
2026-09-16 06:57:41 +0100

    Fix previous deployment bug

--- src/pages/settings/SettingsContext.js
+++ src/pages/settings/SettingsContext.js
@@ -98,7 +98,6 @@ export function SettingsProvider({ children }) {
     };
 
     load();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const value = {
--- src/pages/settings/SettingsDevices.js
+++ src/pages/settings/SettingsDevices.js
@@ -37,7 +37,6 @@ export default function SettingsDevices() {
       }
     };
     load();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleRemove = async (device) => {
